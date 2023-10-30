adb19efdf870f7eb4e56b4c5cab821ba655fda18
Mohcinekhribech <mohcinekhribche123@gmail.com>
2023-10-30 14:32:28 +0100

    instal envirement

---
+++ sunquizz/src/main/java/com/youcode/sunquizz/SunquizzApplication.java
@@ -0,0 +1,13 @@
+package com.youcode.sunquizz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunquizzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunquizzApplication.class, args);
+	}
+
+}
---
+++ sunquizz/src/test/java/com/youcode/sunquizz/SunquizzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youcode.sunquizz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunquizzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
